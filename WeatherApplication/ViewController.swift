285563ff26da6fb38a2e2306c88ceddc051335cf
Dmitry Kniazev <d.knjazev@gmail.com>
2021-08-06 13:54:54 +0300

    Initial Commit

---
+++ WeatherApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApplication
+//
+//  Created by UPIT on 6.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
